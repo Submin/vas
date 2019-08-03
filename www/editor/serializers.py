98590ffed326e30036feca0081d934ed245e2f37
Юрий Шикин <shikin@submin.ru>
2019-08-03 09:29:10 +0400

    add serializers

--- www/editor/serializers.py
+++ www/editor/serializers.py
@@ -13,3 +13,16 @@ class PlatformTypeSerializer(ModelSerializer):
     class Meta:
         model = models.PlatformType
         fields = ['id','name', 'description']
+
+
+class RoutePlatformSerializer(ModelSerializer):
+    class Meta:
+        model = models.RoutePlatform
+        fields = ['prev', 'next', 'name', 'type', 'route', 'longitude', 'latitude', 'description']
+
+
+class RoutePointSerializer(ModelSerializer):
+    class Meta:
+        model = models.RoutePoint
+        fields = ['prev', 'next', 'route', 'longitude', 'latitude', 'description']
+
